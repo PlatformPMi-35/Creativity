24b630d7fdc9204c1d78cb9957ca7a67d49f2415
Володимир Закалик <zakalyk@gmail.com>
2018-09-14 08:38:01 +0300

    Added header sample as .txt

--- Task1/UnitTest/ContactTest.cs
+++ Task1/UnitTest/ContactTest.cs
@@ -1,22 +1,18 @@
-﻿///-----------------------------------------------------------------------
-/// <copyright file="ContactTest.cs" company="Creativity Team">
-///     Company copyright tag.
-/// </copyright>
-///-----------------------------------------------------------------------
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="ContactTest.cs" company="Creativity Team">
+// Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace UnitTest
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
-    /// 
     /// </summary>
     [TestClass]
     public class ContactTest
     {
         /// <summary>
-        /// 
-        /// 
         /// </summary>
         [TestMethod]
         public void TestMethod1()
