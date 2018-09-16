c737e7b498defd03e1c9ac38e72f1e63e283403d
Kamila Hnativ <kamilahnativ1993@gmail.com>
2018-09-16 19:19:34 +0300

    Create class for additional methods

---
+++ Task1/Program/ContactExtentions.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ContactIOManager.cs" company="Creativity Team">
+// (c) <T> inc.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    /// <summary>
+    /// Contains methods to work with Contact hierarchy
+    /// </summary>
+    public static class ContactExtentions
+    {
+
+    }
+}
