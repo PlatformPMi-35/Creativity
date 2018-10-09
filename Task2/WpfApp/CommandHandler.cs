5804aacfdb7b65f726b68b9d535a822387706fbc
Kamila Hnativ <kamilahnativ1993@gmail.com>
2018-10-09 15:00:40 +0300

    Added class CommandHandler

---
+++ Task2/WpfApp/CommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class CommandHandler : ICommand
+    {
+        private Action action;
+        private bool canExecute;
+
+        public CommandHandler(Action action_, bool canExecute_)
+        {
+            this.action = action_;
+            this.canExecute = canExecute_;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.action();
+        }
+    }
+}
