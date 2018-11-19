69cfbf518c3c3e10f75203cf08aebf0bea886590
Volodymyr Zakalyk <34585404+Deohayer@users.noreply.github.com>
2018-11-19 23:02:38 +0200

    Update IOrderValidation.cs

--- Task3/Task3/IOrderValidation.cs
+++ Task3/Task3/IOrderValidation.cs
@@ -8,5 +8,12 @@ namespace Task3
 {
     interface IOrderValidation
     {
+        bool ValidateStreet(string street);
+        bool ValidateHouse(string house);
+        bool ValidatePorch(string porch);
+        bool ValidateName(string name);
+        bool ValidatePhone(string phone);
+        bool ValidateTime(string time);
+        bool ValidateDate(string date);
     }
 }
