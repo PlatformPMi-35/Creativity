8b5d4ccf8eebc3beac174a079ad9774ad76b5b29
Volodymyr Zakalyk <34585404+Deohayer@users.noreply.github.com>
2018-11-19 23:01:52 +0200

    Update OrderValidation.cs
    Implement interface IOrderValidation. Close #66

--- Task3/Task3/OrderValidation.cs
+++ Task3/Task3/OrderValidation.cs
@@ -8,5 +8,41 @@ namespace Task3
 {
     class OrderValidation:IOrderValidation
     {
+        public bool ValidateStreet(string street)
+        {
+            return true;
+        }
+        
+        public bool ValidateHouse(string house)
+        {
+            return true;
+        }
+        
+        public bool ValidatePorch(string porch)
+        {
+            return true;
+        }
+        
+        public bool ValidateName(string name)
+        {
+            Regex regex = new Regex(@"^[A-Za-z]+$");
+            return regex.IsMatch(name);
+        }
+        
+        public bool ValidatePhone(string phone)
+        {
+            Regex regex = new Regex(@"^\d{10}$");
+            return regex.IsMatch(phone);
+        }
+        
+        public bool ValidateTime(string time)
+        {
+            Regex regex = new Regex(@"^\d\d\:\d\d$");
+        }
+        
+        public bool ValidateDate(string date)
+        {
+            return true;
+        }
     }
 }
