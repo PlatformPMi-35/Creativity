4a4a8724651f83e967109827bdcaf17108a19543
Volodymyr Zakalyk <34585404+Deohayer@users.noreply.github.com>
2018-11-19 22:26:29 +0200

    Update OrderConfiguration.cs

--- Task3/Task3/OrderConfiguration.cs
+++ Task3/Task3/OrderConfiguration.cs
@@ -8,5 +8,24 @@ namespace Task3
 {
     class OrderConfiguration:IOrderConfiguration
     {
+        public IOrderFactory GetFactory()
+        {
+            return new OrderFactory;
+        }
+        
+        public IOrderBuilder GetBuilder()
+        {
+            return new OrderBuilder;
+        }
+        
+        public IOrderValidation GetValidator()
+        {
+            return new OrderValidation;
+        }
+        
+        public IDatabaseFacade GetDatabase()
+        {
+            return new DatabaseTxt;
+        }
     }
 }
