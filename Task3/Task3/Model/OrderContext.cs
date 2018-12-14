937a460ee8fad0e5965cde510fea2c4d09dc3354
mariyavisotska <mariyavisotska@gmail.com>
2018-12-14 20:03:59 +0200

    Added class OrderContext, interface IGenericRepository and IUnitOfWork and class GenericRepository.

---
+++ Task3/Task3/Model/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task3.Model
+{
+    public class OrderContext:DbContext
+    {
+        public DbSet<OrderBuilder> Orders { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public OrderContext() : base("name=OrderContext")
+        {
+
+        }
+    }
+}
---
+++ Task3/Task3/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3.Model;
+using Task3.Repository;
+
+namespace Task3.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<Address> DepartmentRepository { get; }
+        GenericRepository<Order> CourseRepository { get; }
+    }
+}
